7b620ab047dd0ceeb99e97344187212ca4c1e5b3
Arailym <230082@astanait.edu.kz>
2023-12-27 08:32:13 +0600

    first init

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class MyApplication {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("C:\Users\User\Desktop\source.txt");
+        File file = new File("C:\\Users\\User\\IdeaProjects\\untitled\\src\\source.txt");
 
         Scanner sc = new Scanner(file);
         Shape shape = new Shape();
